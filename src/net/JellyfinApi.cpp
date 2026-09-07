b6b709f3d8a1133fccae0c50e3d6adc8391c7e78
S3ggie <seggiedel@gmail.com>
2026-09-07 04:52:30 -0500

    refactor: reduce jellyfin api core

--- src/net/JellyfinApi.cpp
+++ src/net/JellyfinApi.cpp
@@ -1,16 +1,1 @@
 #include "JellyfinApi.hpp"
-#include <curl/curl.h>
-#include "HttpClient.hpp"
-#include "../download/HlsPlaylist.hpp"
-#include "../download/HlsProfile.hpp"
-#include "miyoofin/version.hpp"
-#include <cstdio>
-#include <cctype>
-#include <cstdint>
-#include <limits>
-#include <ctime>
-#include <cstdlib>
-
-namespace miyoofin {
-
-} // namespace miyoofin
